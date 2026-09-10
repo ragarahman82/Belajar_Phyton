eb53fe463fbe2ab0611c11c7fc2669d7c7ef67a2
ragarahman82 <ragarahman82@gmail.com>
2026-09-10 10:57:35 +0700

    1_Print.py

---
+++ 1_Print.py
@@ -0,0 +1,66 @@
+print("Saya sedang belajar Python")
+
+#Bedakan antara (10) dan ("10") pada Python, karena (10) adalah integer sedangkan ("10") adalah string.
+print(10 + 5)
+print("10" + "5")
+
+#Contoh
+nama = "Raga"
+umur = 20
+
+print(nama, umur)
+
+print("Nama : Raga")
+print("Umur : 20")
+print("Jurusan : Informatika")
+
+#ini adalah contoh komentar
+print("Halo")
+print("Raga")
+
+#Berbeda dengan ini kalo mau di satu paragraf harus ditambahin end="" di print pertama
+print("Halo", end=" ")
+print("Raga")
+
+#Ada cara lain untuk menambahkan garis baru dengan menambahkan \n di print pertama
+print("Halo\nRaga")
+
+#Ada juga dengan menambahkan \t untuk menambahkan tab di print pertama
+print("Nama\t: Raga")
+print("Umur\t: 20")
+
+#Sekarang kita naik sedikit dengan contoh ini 
+print("=== DATA MAHASISWA ===")
+print("Nama       : Raga")
+print("Umur       : 20")
+print("Jurusan    : Informatika")
+print("=======================")
+
+# Tugas 1 
+             
+print ("======================")
+print ("=== data diri saya ===")
+print ("======================")
+print ("Nama      : Raga")
+print ("Umur      : 20")
+print ("Jurusan   : Informatika")
+print ("Hobi      : Ngoding dan bermain game")
+print ("=====================")
+print ("Saya sedang belajar ngoding python!")
+
+# Tugas 2 
+
+print ("1" + "0")
+print (10 +10)
+print (15 + 15)
+print ("4" + "0")
+print (25 + 25)
+
+# Tugas 3
+print(10 + 5)
+print("10" + "5")
+print("Halo")
+print("Python")
+print("Halo", end="")
+print("Python")
+print("A\nB\nC")
