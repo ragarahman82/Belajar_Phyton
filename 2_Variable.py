be73ee74b25137891fe9af9fa3abf8c328e7be62
ragarahman82 <ragarahman82@gmail.com>
2026-09-11 14:35:35 +0700

    2_Variable.py

---
+++ 2_Variable.py
@@ -0,0 +1,67 @@
+# Variable in Python (Variable di phyton juga salah satu kondep paling penting dalam pemrograman, karena variabel digunakan untuk menyimpan data yang dapat digunakan kembali di dalam program.)
+nama = "Raga"  # String variable
+umur = 25  # Integer variable
+tinggi = 175.5  # Float variable
+jurusan = "Informatika"  # String variable
+
+print(nama)  # Output: Raga
+print(umur)  # Output: 25   
+print(tinggi)  # Output: 175.5
+print(jurusan)  # Output: Informatika
+
+# Variable juga bisa menyimpan angka dan bisa juga dihitung, contohnya seperti ini:
+umur = 30  # Mengubah nilai variabel umur menjadi 30
+print(umur + 1)  # Output: 31
+
+# Variable bisa menyimpan hasil perhitungan, contohnya seperti ini:
+a = 10
+b = 20
+
+hasil = a + b
+
+print(hasil)
+
+# Tugas 1
+nama = "raga"
+umur = 20
+tinggi = 170.5
+jurusan = "Teknik Informatika"
+
+print(nama)
+print(umur)
+print(tinggi)
+print(jurusan)
+
+# Tugas 2
+a = 15
+b = 7
+
+hasil = a + b
+print(hasil)
+
+hasil = a * b
+print(hasil)
+
+hasil = a / b
+print(hasil)
+
+hasil = a - b   
+print(hasil)
+
+# Tugas 3 (Tes Logika)
+x = 10
+y = 5
+
+x = x + y
+
+print(x)
+print(y)
+
+# Tugas 4 (Perubahan Nilai)
+nama = "Raga"
+
+print(nama)
+
+nama = "Budi"
+
+print(nama)
