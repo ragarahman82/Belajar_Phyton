964ce26fb5b040d34881a5501f368ae36cdf7988
ragarahman82 <ragarahman82@gmail.com>
2026-09-16 10:21:34 +0700

    Uji Coba

---
+++ Uji Coba/Percobaan1.py
@@ -0,0 +1,15 @@
+#Tugas 1
+nama = "Raga"
+umur = 20
+tinggi = 170.5
+mahasiswa = True
+
+print(nama)
+print(type(nama))
+print(umur)
+print(type(umur))
+print(tinggi)
+print(type(tinggi))
+print(mahasiswa)
+print(type(mahasiswa))
+
