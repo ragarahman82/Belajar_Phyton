da8fecad4a887f4eb83d032a2946a094502e2fa7
ragarahman82 <ragarahman82@gmail.com>
2026-09-18 19:00:01 +0700

    Input()

---
+++ 4_Input().py
@@ -0,0 +1,22 @@
+#Dengan input(), pengguna bisa memasukkan data ketika program sedang berjalan.
+nama = input("Masukkan nama Anda: ")
+umur = int(input("Masukkan umur Anda: "))   
+jurusan = input("Masukkan jurusan Anda: ")
+
+print("Nama:", nama)
+print("Umur:", umur)
+print("Jurusan:", jurusan)
+
+
+angka_pertama = int(input("Masukkan angka pertama: "))
+angka_kedua = int(input("Masukkan angka kedua: "))
+penjumlahan = angka_pertama + angka_kedua
+pengurangan = angka_pertama - angka_kedua
+perkalian = angka_pertama * angka_kedua
+
+print("Hasil penjumlahan:", penjumlahan)
+print("Hasil pengurangan:", pengurangan)
+print("Hasil perkalian:", perkalian)
+
+    
+
---
+++ Uji Coba/Percobaan2.py
@@ -0,0 +1,7 @@
+nama = input("Nama: ")
+
+print(type(nama))
+
+umur = input("Umur: ")
+
+print(umur + 5)
