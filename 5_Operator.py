7a9ba3d27dba7b73d2601b375113074758244119
ragarahman82 <ragarahman82@gmail.com>
2026-09-19 19:36:11 +0700

    Operator

---
+++ 5_Operator.py
@@ -0,0 +1,22 @@
+#Sekarang kita masuk ke operator. Ini penting karena nanti hampir semua program menggunakan operator.
+a = 20
+b = 6
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+a = 17
+b = 5
+
+print(a // b)
+print(a % b)
+
+a = 10
+b = 11
+print(a % 2)
+print(b % 2)
