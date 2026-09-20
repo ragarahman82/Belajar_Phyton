3daaf92859e23d6c64861e2630dc95b759d228f2
ragarahman82 <ragarahman82@gmail.com>
2026-09-20 12:43:36 +0700

    If&Else

--- 6_If&Else.py
+++ 6_If&Else.py
@@ -20,3 +20,21 @@ if x > 5:
     print("A")
 else:
     print("B")
+
+#IF → kondisi benar, ELSE → kondisi salah
+#Tapi bagaimana kalau kita punya lebih dari 2 kondisi? maka kita gunakan ELIF (else if) untuk menambahkan kondisi lain. Contohnya seperti ini:
+nilai = 85
+
+if nilai >= 90:
+    print("Nilai A")
+elif nilai >= 80 and nilai <= 89:
+    print("Nilai B")
+elif nilai >= 70 and nilai <= 79:
+    print("Nilai C")
+elif nilai >= 60 and nilai <= 69:
+    print("Nilai D")
+elif nilai >= 50:
+    print("Nilai E")
+else:
+    print("Nilai D atau E")
+
