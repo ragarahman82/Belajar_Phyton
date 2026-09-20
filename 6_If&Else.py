84856bb47e9f83dd2f0a1bd422b5694b72d3234a
ragarahman82 <ragarahman82@gmail.com>
2026-09-20 09:02:09 +0700

    If&Else

---
+++ 6_If&Else.py
@@ -0,0 +1,22 @@
+#Pengambilan keputusan dengan if dan else
+angka = 8
+angka = 7
+
+if angka % 2 == 0:
+    print("maka angka genap")
+else:
+    print("maka angka ganjil")
+
+umur = 18
+
+if umur >= 17:
+    print("Sudah cukup umur")
+else:
+    print("Belum cukup umur")
+
+x = 10
+
+if x > 5:
+    print("A")
+else:
+    print("B")
