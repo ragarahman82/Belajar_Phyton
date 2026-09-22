02afd1e05973fc267c5d14862860f58d73f1a724
ragarahman82 <ragarahman82@gmail.com>
2026-09-22 08:21:01 +0700

    If&Else

---
+++ Uji Coba/Percobaan3.py
@@ -0,0 +1,31 @@
+umur = 20
+nilai = 80
+
+if umur >= 17 and nilai >= 75:
+    print("Lulus")
+else:
+    print("Tidak lulus")
+
+hari = "Minggu"
+
+if hari == "Sabtu" or hari == "Minggu":
+    print("Libur")
+else:
+    print("Kuliah")
+
+saldo = 5000
+harga = 10000
+
+if saldo >= harga:
+    print("Bisa membeli")
+else:
+    print("Saldo tidak cukup")
+
+umur = 18
+tiket = "True"
+
+if umur >= 18 and tiket:
+    print("Boleh masuk")
+else:
+    print("Tidak boleh masuk")
+    
