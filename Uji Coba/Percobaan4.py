0c625bfe9c8beee418d44a5fe09513e15c8616b5
ragarahman82 <ragarahman82@gmail.com>
2026-09-23 10:13:54 +0700

    Percobaan4

--- Uji Coba/Percobaan4.py
+++ Uji Coba/Percobaan4.py
@@ -8,3 +8,35 @@ if umur >= 20:
         print("Tidak punya KTM")
 else:
     print("Umur belum cukup")
+
+umur = 17
+punya_ktm = True
+
+if umur >= 18:
+    if punya_ktm:
+        print("Boleh masuk")
+    else:
+        print("Tidak punya KTM")
+else:
+    print("Umur belum cukup")
+
+umur = 20
+punya_ktm = False
+
+if umur >= 18:
+    if punya_ktm:
+        print("Boleh masuk")
+    else:
+        print("Tidak punya KTM")
+else:
+    print("Umur belum cukup")
+
+nilai = 80
+kehadiran = 85
+
+if nilai >= 75 and kehadiran >= 80:
+    print("Boleh mengikuti ujian")
+else:
+    print("Tidak boleh mengikuti ujian")
+
+    
