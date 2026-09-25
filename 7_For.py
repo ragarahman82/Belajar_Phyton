fb488a5e94c4d5e4f894b182f3796df17241b122
ragarahman82 <ragarahman82@gmail.com>
2026-09-25 19:46:02 +0700

    for

---
+++ 7_For.py
@@ -0,0 +1,12 @@
+#Perulangan digunakan ketika kita ingin menjalankan kode berulang kali.
+for i in range(5):
+    print(i)
+
+for i in range(1,6):
+    print(i)
+
+for i in range(1,11):
+    print(i)
+
+for i in range(11,1):
+    print(i)
