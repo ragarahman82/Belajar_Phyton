657c237ce11fc214e517a5a7ee5d4549bab17fb0
ragarahman82 <ragarahman82@gmail.com>
2026-09-25 19:54:30 +0700

    For

--- 7_For.py
+++ 7_For.py
@@ -10,3 +10,15 @@ for i in range(1,11):
 
 for i in range(11,1):
     print(i)
+
+#range(start, stop, step)
+for i in range(1, 11, 2):
+    print(i)
+
+for i in range(10, 0, -1):
+    print(i)
+
+#Nah, sekarang kita gabungkan materi lama (if) dengan materi baru (for).
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
