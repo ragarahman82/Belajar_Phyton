16337944d18ae7ac9d722a7a3eadc7b1e4d0d08e
ragarahman82 <ragarahman82@gmail.com>
2026-09-26 11:35:37 +0700

    Percobaan5

---
+++ Uji Coba/Percobaan5.py
@@ -0,0 +1,18 @@
+for i in range(1, 11):
+    print(i)
+
+for i in range(10, 0, -1):
+    print(i)
+
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+
+for i in range(1, 21, 2):
+    print(i)
+
+for i in range(1, 6):
+    print("raga Belajar Phyton")
+
+for i in range(1, 11):
+    print("5 x", i, "=", 5 * i)
